f2f06d6b0c6f7b7716c95179be7460be3d2ad624
ricksonl.berigo <ricksonl.berigo@outlook.com>
2023-05-24 16:36:18 -0300

    Iniciando com o projeto e fazendo a primeira busca

---
+++ buscar-cursos.php
@@ -0,0 +1,20 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client();
+$response = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+
+$html = $response->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$courses = $crawler->filter('span.card-curso__nome');
+
+foreach ($courses as $course) {
+  echo $course->textContent . PHP_EOL;
+}
