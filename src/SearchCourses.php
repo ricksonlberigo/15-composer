1d8be4f1ba6ad599e3ef7da3cd1c5900960aca04
ricksonl.berigo <ricksonl.berigo@outlook.com>
2023-05-30 09:28:43 -0300

    Adicionando no nosso projeto em ambiente de desenvolvimento o PHPCS

--- src/SearchCourses.php
+++ src/SearchCourses.php
@@ -7,26 +7,26 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class SearchCourses
 {
-  public function __construct(
-    private ClientInterface $httpClient,
-    private Crawler $crawler
-  ) {
-  }
-
-  public function search(string $url): array
-  {
-    $response = $this->httpClient->request("GET", $url);
-
-    $html = $response->getBody();
-    $this->crawler->addHtmlContent($html);
-
-    $courseElement = $this->crawler->filter('span.card-curso__nome');
-    $courses = [];
-
-    foreach ($courseElement as $element) {
-      $courses[] = $element->textContent;
+    public function __construct(
+        private ClientInterface $httpClient,
+        private Crawler $crawler
+    ) {
     }
 
-    return $courses;
-  }
+    public function search(string $url): array
+    {
+        $response = $this->httpClient->request("GET", $url);
+
+        $html = $response->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $courseElement = $this->crawler->filter('span.card-curso__nome');
+        $courses = [];
+
+        foreach ($courseElement as $element) {
+            $courses[] = $element->textContent;
+        }
+
+        return $courses;
+    }
 }
